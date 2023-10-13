42e96824e750eb6fc84ee31bfe77dc7171c2c306
JirapunJunruk <jirapun0907@gmail.com>
2023-10-13 16:17:37 +0700

    update

---
+++ local/src/App.js
@@ -0,0 +1,10 @@
+//import logo from './logo.svg';
+//import './App.css';
+import React from 'react';
+import FormPost from './form-post';
+
+function App() {
+  return <FormPost/>
+}
+
+export default App;
